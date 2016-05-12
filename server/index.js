f86c2d9392f049589e5163409069b43d0450ede9
Rob Quist <rob.quist@dukesoft.nl>
2016-05-12 23:55:05 +0200

    Restructure

--- server/index.js
+++ server/index.js
@@ -3,27 +3,24 @@ var io = require("socket.io").listen(31415);
 io.sockets.on("connection", function (socket) {
     console.log('A Client has Connected to this Server');
 
-    socket.on("getAllActions", function (data) {
+    socket.on("getAllModules", function (data) {
         console.log('Asking all actions!', data);
-        socket.emit('defineAllActions',
-            { actions:
-                [
-                    {
-                        name: 'lights',
-                        functions: {
-                            powerControl: {
-                                arguments: 1,
-                                argumentDefinition: [
-                                    {
-                                        type: 'bool',
-                                        name: 'enabled'
-                                    }
-                                ]
+        socket.emit('defineAllModules',
+            [
+                {
+                    name: 'kakuLights',
+                    functions: [
+                            {
+                                name: 'powerControl',
+                                arguments: [{type:'bool', name:'enabled'}]
+                            },
+                            {
+                                name: 'dim',
+                                arguments: [{type:'bit', name:'dimmingLevel'}]
                             }
-                        }
-                    }
-                ]
-            }
+                    ]
+                }
+            ]
         );
     });
 
