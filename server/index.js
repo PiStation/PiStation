a13fadb527e38ea41ddd7974995012fa13277ced
Rob Quist <rob.quist@dukesoft.nl>
2016-05-12 23:47:06 +0200

    add mock response for getAllActions event

--- server/index.js
+++ server/index.js
@@ -3,8 +3,28 @@ var io = require("socket.io").listen(31415);
 io.sockets.on("connection", function (socket) {
     console.log('A Client has Connected to this Server');
 
-    socket.on("message", function (data) {
-        console.log('Message!', data);
+    socket.on("getAllActions", function (data) {
+        console.log('Asking all actions!', data);
+        socket.emit('defineAllActions',
+            { actions:
+                [
+                    {
+                        name: 'lights',
+                        functions: {
+                            powerControl: {
+                                arguments: 1,
+                                argumentDefinition: [
+                                    {
+                                        type: 'bool',
+                                        name: 'enabled'
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                ]
+            }
+        );
     });
 
     socket.on("disconnect", function (data) {
