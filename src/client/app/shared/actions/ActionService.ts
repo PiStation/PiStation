6ac558f631d3a41acde85d991556aa34b369324b
Robbert van Markus <robbert@vanmarkus.com>
2016-05-14 00:28:01 +0200

    moved server directory to src folder, moved files to root

--- src/client/app/shared/actions/ActionService.ts
+++ src/client/app/shared/actions/ActionService.ts
@@ -1,5 +1,7 @@
+/// <reference path="../../../socket.io.d.ts"/>
 import {Observable} from "rxjs/Observable";
-import * as PiStation from '../../../../../../PiStation.ts'
+import * as PiStation from '../../../../../PiStation.ts'
+import {Observer} from "rxjs/Observer";
 
 export class ActionService {
     private socket : SocketIOClient.Socket;
@@ -11,8 +13,8 @@ export class ActionService {
 
     getAllActions() : Observable<PiStation.Module[]> {
         this.socket.emit('getAllModules');
-        return Observable.create(observer => {
-            this.socket.on('defineAllModules', (data) => observer.next(data));
+        return Observable.create((observer : any) => {
+            this.socket.on('defineAllModules', (data : PiStation.Module[]) => observer.next(data));
         });
     }
 }
