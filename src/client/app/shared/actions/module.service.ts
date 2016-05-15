7ba542e5ed00a32dc4b85243cdaef72ac19d932b
Robbert van Markus <robbert@vanmarkus.com>
2016-05-15 22:19:21 +0200

    sendModuleFunction

--- src/client/app/shared/actions/module.service.ts
+++ src/client/app/shared/actions/module.service.ts
@@ -3,16 +3,18 @@ import * as PiStation from '../../../PiStation';
 import * as Rx from 'rxjs/Rx';
 
 @Injectable() export class ModuleService {
-    private socket : SocketIOClient.Socket;
-
-    constructor() {
-        this.socket = io('http://localhost:31415');
-        //console.log(new PiStation.Argument('bla', 'bloe'));
-    }
+    private socket : SocketIOClient.Socket = io.connect('http://localhost:31415');
 
     getAllModules() : Rx.Observable<PiStation.Module[]> {
         this.socket.emit(PiStation.Events.GET_ALL_MODULES);
         return Rx.Observable.fromEvent<PiStation.Module[]>(this.socket, PiStation.Events.GET_ALL_MODULES);
     }
-}
 
+    sendModuleFunction(module : PiStation.Module, func : PiStation.Function, args: PiStation.Argument[] = []) {
+        this.socket.emit(this.getEventNameForModuleFunction(module, func), args);
+    }
+
+    private getEventNameForModuleFunction(module : PiStation.Module, func : PiStation.Function) {
+        return `${module.name}:${func.name}`;
+    }
+}
